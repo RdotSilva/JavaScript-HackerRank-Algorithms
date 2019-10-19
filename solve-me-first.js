29ab16b399d533703a51618110fb824c12646f90
Ryan Silva <RyandotSilva@gmail.com>
2019-10-19 16:26:13 -0400

    Add solution

---
+++ solve-me-first.js
@@ -0,0 +1,6 @@
+//https://www.hackerrank.com/challenges/solve-me-first/problem
+//Complete the function solveMeFirst to compute the sum of two integers.
+
+const solveMeFirst = (a, b) => {
+	return a + b;
+};
